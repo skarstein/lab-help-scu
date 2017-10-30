a0721623ac470af17dff170369e89f6c1f4ba58d
ngoodpaster <ngoodpaster@scu.edu>
2017-10-30 00:22:04 -0700

    updated student login form

--- qa_student.php
+++ qa_student.php
@@ -12,18 +12,18 @@
           <h1>TA Help Seeking System</h1>
         </div>
       </div>
-      <form role="form">
+      <form role="form" action = "qa_student.php" method ="post">
         <div class="form-group">
           <label for="usernameInput">Username</label>
-          <input type="text" class="form-control" id="usernameInput" placeholder="Enter Username">
+          <input type="text" class="form-control" name="usernameInput" placeholder="Enter Username">
         </div>
         <div class="form-group">
           <label for="passwordInput">Password</label>
-          <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+          <input type="password" class="form-control" name="passwordInput" placeholder="Password">
         </div>
         <div class="form-group">
           <label for="sessionIdInput">Session Id</label>
-          <input type="password" class="form-control" id="sessionIdInput" placeholder="Session Id">
+          <input type="password" class="form-control" name="sessionIdInput" placeholder="Session Id">
         </div>
         <!-- <div class="form-group">
           <label for="exampleInputFile">File input</label>
@@ -37,9 +37,32 @@
         </div> -->
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
+      <p id = "p"></p>  
     </div>
 
+    <?php 
+        session_start();
+        $_SESSION["username"] = $_POST["usernameInput"];
+        $_SESSION["sessionId"] =$_POST["sessionIdInput"];
+    ?>
 
+    Welcome <?php echo $_SESSION["username"]; ?><br>
+    Your session is: <?php echo $_SESSION["sessionId"]; ?> 
+
+
+<!--    <script>
+        var p = document.getElementById("p")
+        var password = document.getElementById("passwordInput")
+        var username = document.getElementById("usernameInput")
+        var sessionId = document.getElementById("sessionIdInput")
+
+        function handleSubmitButtonClick(){
+            p.innerHTML = username + " : " + password + " : " + sessionId
+            //Check if credentials are accurate
+            //Hide Form, and load Questions
+            //
+        }
+    </script> -->
   </body>
 
 </html>
