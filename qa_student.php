7634cf0cfe0c243f206a7772ecf946f130c6865c
ngoodpaster <ngoodpaster@scu.edu>
2017-10-29 18:49:09 -0700

    added form to qa_student

--- qa_student.php
+++ qa_student.php
@@ -12,7 +12,33 @@
           <h1>TA Help Seeking System</h1>
         </div>
       </div>
+      <form role="form">
+        <div class="form-group">
+          <label for="usernameInput">Username</label>
+          <input type="text" class="form-control" id="usernameInput" placeholder="Enter Username">
+        </div>
+        <div class="form-group">
+          <label for="passwordInput">Password</label>
+          <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <label for="sessionIdInput">Session Id</label>
+          <input type="password" class="form-control" id="sessionIdInput" placeholder="Session Id">
+        </div>
+        <!-- <div class="form-group">
+          <label for="exampleInputFile">File input</label>
+          <input type="file" id="exampleInputFile">
+          <p class="help-block">Example block-level help text here.</p>
+        </div> -->
+<!--         <div class="checkbox">
+          <label>
+            <input type="checkbox"> Check me out
+          </label>
+        </div> -->
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     </div>
+
 
   </body>
 
