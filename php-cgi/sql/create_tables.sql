bb8c1b7f60c6e139f04995e66d30c664e78f21e9
skarstein <skarstei@linux60410.dc.engr.scu.edu>
2017-11-01 20:24:30 -0700

    fixed database

--- php-cgi/sql/create_tables.sql
+++ php-cgi/sql/create_tables.sql
@@ -1,9 +1,16 @@
 CREATE TABLE User(
     username varchar(20),
-    salt varchar(40),
-    password varchar(40),
+    salt varchar(22),
+    password varchar(60),
     type char(2), CHECK (type IN ('ST','TA')),
     PRIMARY KEY (username)
+);
+
+CREATE TABLE Session(
+    s_id varchar(10),
+    class_name varchar(10),
+    t_stamp timestamp,
+    PRIMARY KEY (s_id)
 );
 
 CREATE TABLE Question(
@@ -15,13 +22,7 @@ CREATE TABLE Question(
     answer_content varchar(1000),
     PRIMARY KEY (q_id),
     FOREIGN KEY(s_id) REFERENCES Session(s_id),
-    FOREIGN KEY(username) REFERENCES User(username)
+    FOREIGN KEY(username) REFERENCES User(username) ON DELETE CASCADE
 );
 
-CREATE TABLE Session(
-    s_id varchar(10),
-    class_name varchar(10),
-    t_stamp timestamp,
-    PRIMARY KEY (s_id)
-);
 
