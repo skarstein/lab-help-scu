d97aebcdff24d575ce70a6d139c6745fe682952f
skarstein <skarstei@linux60411.dc.engr.scu.edu>
2017-11-09 16:04:24 -0800

    sorting for ta

--- php-cgi/ta.php
+++ php-cgi/ta.php
@@ -39,21 +39,46 @@
       <div class="row">
         <div class="col-12">
           <?php
-          $sql = "SELECT * FROM Question WHERE (s_id = '" .$_SESSION["sessionID"]. "') ORDER BY t_stamp DESC";
-          $result = $conn->query($sql);
-          ?>
+            $sql = "SELECT * FROM Question WHERE s_id='" .$_SESSION['sessionID']. "' ORDER BY ";
 
+
+
+        if (!empty($_GET['sort'])){// == 'time')
+            $sort = $_GET['sort'];
+        } else {
+            $sort = "t_stamp";
+        }
+
+        if (!empty($_GET['dir'])){
+            $dir = $_GET['dir'];
+        } else {
+            $dir = "DESC";
+        }
+
+        $sql .= $sort. " " .$dir;
+
+        if ($dir == "DESC"){
+            $dir = "ASC";
+        } else {
+            $dir = "DESC";
+        }
+
+        $result = $conn->query($sql);
+
+        echo '
           <table class="table">
             <thead>
               <tr>
-                <th>Student</th>
-                <th>Time</th>
-                <th>Question</th>
-                <th>Answer</th>
+                <th><a href="ta.php?sort=username&dir='.$dir.'">Student</th>
+                <th><a href="ta.php?sort=t_stamp&dir='.$dir.'">Time</th>
+                <th><a href="ta.php?sort=question_content&dir='.$dir.'">Question</th>
+                <th><a href="ta.php?sort=answer_content&dir='.$dir.'">Answer</th>
+
               </tr>
             </thead>
             <tbody>
-            <?php
+            ';
+
             while($row = $result->fetch_assoc()){
               echo 
                 "<tr>
